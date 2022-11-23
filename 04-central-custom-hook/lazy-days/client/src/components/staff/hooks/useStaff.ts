52fb37f9d80aed1b10d16f2d1c4065bc4081e468
Noah Kim <dongwoo_333@hanmail.net>
2022-11-24 07:50:49 +1100

    51. Code Quiz! Selector for useStaff

--- 04-central-custom-hook/lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ 04-central-custom-hook/lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -22,9 +22,16 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
+  const selectFn = useCallback(
+    (unfilteredStaff: Staff[]) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
+
   // const staff = [];
   const fallback = [];
-  const { data: staff = fallback } = useQuery([queryKeys.staff], getStaff);
+  const { data: staff = fallback } = useQuery([queryKeys.staff], getStaff, {
+    select: filter !== 'all' ? selectFn : undefined,
+  });
 
   return { staff, filter, setFilter };
 }
