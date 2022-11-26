1b9d0e095f8174f3624a9a1198571ea301d0f4b1
Noah Kim <dongwoo_333@hanmail.net>
2022-11-27 09:12:35 +1100

    83. Code Quiz! Staff Query Errors

--- 04-central-custom-hook/lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ 04-central-custom-hook/lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,10 @@
-import { screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
 
-// import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
+import { generateQueryClient } from '../../../react-query/queryClient';
 import { renderWithClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -18,9 +20,43 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.use(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+
+  // set up query client with retries set to false
+  const productionClient = generateQueryClient();
+  const cache = productionClient.getQueryCache();
+  const options = productionClient.getDefaultOptions();
+  // in this react-query v4, the test just passes without retry option
+  options.queries = { ...options.queries, retry: false };
+
+  const client = new QueryClient({
+    queryCache: cache,
+    defaultOptions: options,
+    logger: {
+      // eslint-disable-next-line no-console
+      log: console.log,
+      // eslint-disable-next-line no-console
+      warn: console.warn,
+      error: () => {
+        // swallow errors without printing out
+        // just to make the console clear for test
+        // as they are intended error messages, there's no need to print them
+      },
+    },
+  });
+
+  // render wrapped with provider
+  render(
+    <QueryClientProvider client={client}>
+      <AllStaff />
+    </QueryClientProvider>,
+  );
+
+  // check for toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
