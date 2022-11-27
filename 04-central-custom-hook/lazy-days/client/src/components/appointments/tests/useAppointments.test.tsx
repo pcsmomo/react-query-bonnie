c9d458cce9f9700b37a4fc275a7a2911d97fbfce
Noah Kim <dongwoo_333@hanmail.net>
2022-11-28 07:18:47 +1100

    88. Test Appointments Filter

--- 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,8 +1,29 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
 
 test('filter appointments by availability', async () => {
-  // test goes here
+  const { result, waitFor } = renderHook(useAppointments, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // to get your bearings
+  console.log(result);
+  console.log(result.current);
+
+  // wait for the appointments to populate
+  await waitFor(() => Object.keys(result.current.appointments).length > 0);
+
+  const filteredAppointsLength = Object.keys(result.current.appointments)
+    .length;
+
+  // set to show all appointments
+  act(() => result.current.setShowAll(true));
+
+  // await for the appointments to show more than when filtered
+  await waitFor(
+    () =>
+      Object.keys(result.current.appointments).length > filteredAppointsLength,
+  );
 });
