bc2499e2e1f5892ca863e6ed429cc8427cda3b3d
Noah Kim <dongwoo_333@hanmail.net>
2022-11-28 06:56:29 +1100

    85. Code Quiz! Test Cancel Appointment Mutation

--- 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -41,9 +41,26 @@ test('Reserve appointment', async () => {
 });
 
 test('Cancel appointment', async () => {
-  // your test here
-  //
-  // const cancelButtons = await screen.findAllByRole('button', {
-  //   name: /cancel appointment/i,
-  //  });
+  renderWithClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
+  // fin all the cancel buttons
+  const cancelButtons = await screen.findAllByRole('button', {
+    name: /cancel appointment/i,
+  });
+
+  // click on the first one to reserve
+  fireEvent.click(cancelButtons[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('cancel');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
