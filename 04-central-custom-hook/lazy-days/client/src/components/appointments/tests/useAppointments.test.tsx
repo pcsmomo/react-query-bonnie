49575e9d25f7565711631ef5e9ff6d7bf374d69a
Noah Kim <dongwoo_333@hanmail.net>
2022-11-28 07:19:13 +1100

    87. PLEASE READ: update to the following lecture

--- 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ 04-central-custom-hook/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -2,6 +2,15 @@ import { act, renderHook } from '@testing-library/react-hooks';
 
 import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
+import { AppointmentDateMap } from '../types';
+
+// a helper function to get the total number of appointments from an AppointmentDateMap object
+const getAppointmentCount = (appointments: AppointmentDateMap) =>
+  Object.values(appointments).reduce(
+    (runningCount, appointmentsOnDate) =>
+      runningCount + appointmentsOnDate.length,
+    0,
+  );
 
 test('filter appointments by availability', async () => {
   const { result, waitFor } = renderHook(useAppointments, {
@@ -9,14 +18,15 @@ test('filter appointments by availability', async () => {
   });
 
   // to get your bearings
-  console.log(result);
-  console.log(result.current);
+  // console.log(result);
+  // console.log(result.current);
 
   // wait for the appointments to populate
-  await waitFor(() => Object.keys(result.current.appointments).length > 0);
+  await waitFor(() => getAppointmentCount(result.current.appointments) > 0);
 
-  const filteredAppointsLength = Object.keys(result.current.appointments)
-    .length;
+  const filteredAppointsLength = getAppointmentCount(
+    result.current.appointments,
+  );
 
   // set to show all appointments
   act(() => result.current.setShowAll(true));
@@ -24,6 +34,6 @@ test('filter appointments by availability', async () => {
   // await for the appointments to show more than when filtered
   await waitFor(
     () =>
-      Object.keys(result.current.appointments).length > filteredAppointsLength,
+      getAppointmentCount(result.current.appointments) > filteredAppointsLength,
   );
 });
